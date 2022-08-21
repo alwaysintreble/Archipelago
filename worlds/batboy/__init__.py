35040fa71619e84cb254a6b0fd64494e877e7ce6
alwaysintreble <mmmcheese158@gmail.com>
2022-08-21 17:02:58 -0500

    set up basic web world

---
+++ worlds/batboy/__init__.py
@@ -0,0 +1,21 @@
+﻿from ..AutoWorld import WebWorld, World
+from BaseClasses import MultiWorld, Tutorial
+
+
+class BatBoyWeb(WebWorld):
+    tut_en = Tutorial(
+        "setup",
+        "Tutorial on setting up the batboy client for archipelago play.",
+        "English",
+        "setup_en.md",
+        "setup_en",
+        ["alwaysintreble"],
+    )
+    tutorials = [tut_en]
+    theme = "grassFlowers"
+
+
+class BatBoyWorld(World):
+    web = BatBoyWeb()
+
+
