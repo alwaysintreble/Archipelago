1b4c161ac8e382005eb88956175b271023e4d08a
alwaysintreble <mmmcheese158@gmail.com>
2022-08-22 19:17:06 -0500

    items and locations files to fill out name_to_id dicts

---
+++ worlds/batboy/Items.py
@@ -0,0 +1,21 @@
+﻿from typing import Dict
+
+from BaseClasses import Item
+
+from .Constants.ItemsAndLocations import ITEM_NAMES, ABILITY_NAMES
+
+
+class BatBoyItem(Item):
+    game = "BatBoy"
+
+
+base_offset = 696969
+ability_offset = 20
+
+
+def item_name_to_id() -> Dict[str, int]:
+    items = {item_name: item_id for item_id, item_name in enumerate(ITEM_NAMES, base_offset)}
+    abilities = {ability_name: ability_id
+                 for ability_id, ability_name in enumerate(ABILITY_NAMES, (base_offset + ability_offset))}
+    print(items | abilities)
+    return items | abilities
---
+++ worlds/batboy/Locations.py
@@ -0,0 +1,28 @@
+﻿from typing import Dict, List
+from BaseClasses import Location
+
+from .Constants.RegionConstants import LEVEL_TO_HINT_NAMES
+from .Constants.ItemsAndLocations import LOCATION_NAMES, SHOP_NAMES
+
+
+class BatBoyLocation(Location):
+    game = "BatBoy"
+
+
+base_offset = 696969
+shop_offset = 200
+
+
+def location_name_to_id() -> Dict[str, int]:
+    loc_names: List[str] = []
+    for region_name in LEVEL_TO_HINT_NAMES:
+        for loc_name in LOCATION_NAMES:
+            loc_names.append(region_name + " " + loc_name)
+
+    loc_map: Dict[str, int] = {loc_name: loc_id for loc_id, loc_name in enumerate(loc_names, base_offset)}
+
+    # the demo only has a single shop so this is pseudo hard coded until i find a better way to handle shops
+    shop_map: Dict[str, int] = {shop_slot: shop_id
+                                for shop_id, shop_slot in enumerate(SHOP_NAMES, (base_offset + shop_offset))}
+    print(loc_map | shop_map)
+    return loc_map | shop_map
