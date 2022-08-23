d400b5ea1cc66eca1dfaf77a1f886bcabdb3ab91
alwaysintreble <mmmcheese158@gmail.com>
2022-08-22 19:16:33 -0500

    combine location and item constant files

---
+++ worlds/batboy/Constants/ItemsAndLocations.py
@@ -0,0 +1,37 @@
+﻿
+# all lookup on client is done by ID so it's very important all of these keep their order
+ITEM_NAMES = [
+    "Red Seed",
+    "Green Seed",
+    "Golden Seed",
+    "Increase HP",
+    "Increase Stamina"
+]
+
+ABILITY_NAMES = [
+    "Bat Spin",
+    "Slash Bash",
+    "Grappling Ribbon",
+]
+#    "Bubble Shield", TODO wait for the game to release :)
+#    "Bull Rush",
+#    "Wall Jump",
+#    "Bouncing Basket",
+#    "Mega Smash",
+#    "Ace Stomp"
+#]
+
+LOCATION_NAMES = [
+    "Red Seed",
+    "Green Seed",
+    "Golden Seed",
+    # "Cassette", TODO maybe possible?
+    "Level Clear",
+]
+
+SHOP_NAMES = [
+    "Shop Slot 1",
+    "Shop Slot 2",
+    "Shop Slot 3",
+    "Shop Consumable Item",
+]
