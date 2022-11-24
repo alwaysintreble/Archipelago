90c422d7884c112fa94fef6ef9687132f523ab20
alwaysintreble <mmmcheese158@gmail.com>
2022-11-23 18:34:50 -0600

    self.multiworld

--- worlds/batboy/__init__.py
+++ worlds/batboy/__init__.py
@@ -94,7 +94,7 @@ class BatBoyWorld(Auto.World):
                 if new_name in location_rules:
                     rule = location_rules[new_name]
                 self.create_location(new_name, level, rule)
-            self.world.regions.append(level)
+            self.multiworld.regions.append(level)
 
         for level_name in CASSETTE_ONLY_REGIONS:
             level = self.create_region(level_name)
@@ -104,7 +104,7 @@ class BatBoyWorld(Auto.World):
             new_name = f"{level_name} Cassette"
             self.create_location(new_name, level, None)
 
-            self.world.regions.append(level)
+            self.multiworld.regions.append(level)
 
     def create_shops(self) -> None:
         shop_rules = self.batboy_rules.shop_rules
@@ -119,17 +119,17 @@ class BatBoyWorld(Auto.World):
             if loc in shop_rules:
                 rule = shop_rules[loc]
             self.create_location(loc, shop, rule)
-        self.world.regions.append(shop)
+        self.multiworld.regions.append(shop)
 
     def create_region(self, name: str) -> Region:
         if name in LEVEL_TO_HINT_NAMES:
-            return Region(name, RegionType.Generic, LEVEL_TO_HINT_NAMES[name], self.player, self.world)
-        return Region(name, RegionType.Generic, name, self.player, self.world)
+            return Region(name, RegionType.Generic, LEVEL_TO_HINT_NAMES[name], self.player, self.multiworld)
+        return Region(name, RegionType.Generic, name, self.player, self.multiworld)
 
     def create_regions(self) -> None:
         # create and connect our menu and overworld regions before anything else
-        menu = Region("Menu", RegionType.Generic, "Menu", self.player, self.world)
-        self.overworld = Region("Overworld", RegionType.Generic, "Overworld", self.player, self.world)
+        menu = Region("Menu", RegionType.Generic, "Menu", self.player, self.multiworld)
+        self.overworld = Region("Overworld", RegionType.Generic, "Overworld", self.player, self.multiworld)
         start = Entrance(self.player, "Game Start", menu)
         menu.exits.append(start)
         start.connect(self.overworld)
@@ -139,33 +139,33 @@ class BatBoyWorld(Auto.World):
         # create a region for each level and connect it to the overworld with rules
         self.create_levels()
         self.create_shops()
-        self.world.regions += [menu, self.overworld]
+        self.multiworld.regions += [menu, self.overworld]
 
     def generate_basic(self) -> None:
         # place bat spin in the first level and remove it from the item pool
-        possible_locs = [loc for loc in self.world.get_region("Grassy Plains", self.player).locations]
-        possible_locs.remove(self.world.get_location("Grassy Plains Golden Seed", self.player))
-        possible_locs.append(self.world.get_location("Shop Slot 1", self.player))
-        loc = self.world.random.choice(possible_locs)
+        possible_locs = [loc for loc in self.multiworld.get_region("Grassy Plains", self.player).locations]
+        possible_locs.remove(self.multiworld.get_location("Grassy Plains Golden Seed", self.player))
+        possible_locs.append(self.multiworld.get_location("Shop Slot 1", self.player))
+        loc = self.multiworld.random.choice(possible_locs)
 
         loc.place_locked_item(self.create_item("Bat Spin"))
 
         # each level has 3 seeds, cassette, and the level clear
         # each shop has 3 item slots and a consumable item slot
         # subtract 1 due to above item placement
-        locations_len: int = len(self.world.get_unfilled_locations(self.player))
+        locations_len: int = len(self.multiworld.get_unfilled_locations(self.player))
 
         # check if the itempool and number of locations are equal and if not make them equal with red seeds
         while len(self.itempool) < locations_len:
             self.itempool.append("Red Seed")
 
         itempool = [self.create_item(item_name) for item_name in self.itempool]
-        self.world.itempool += itempool
+        self.multiworld.itempool += itempool
 
     def set_rules(self) -> None:
-        self.world.completion_condition[self.player] = lambda state: state.has_all(ABILITY_NAMES, self.player)
+        self.multiworld.completion_condition[self.player] = lambda state: state.has_all(ABILITY_NAMES, self.player)
 
     def fill_slot_data(self) -> Dict[str, Any]:
         return {
-            "deathlink": self.world.death_link[self.player].value,
+            "deathlink": self.multiworld.death_link[self.player].value,
         }
