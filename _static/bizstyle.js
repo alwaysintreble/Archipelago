0437dfd681847d6f0cf7e665d6b64b574b7cbe48
alwaysintreble <alwaysintreble@users.noreply.github.com>
2025-04-18 22:57:20 +0000

    deploy: 47cd055e34373eadc189223d1e15449bc29e7092

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -17,7 +17,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Archipelago 0.6.0 documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Archipelago 0.6.2 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
