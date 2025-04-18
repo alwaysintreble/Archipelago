2490e8badc8507f31f1037f0d1c9a01c2faffdc9
alwaysintreble <mmmcheese158@gmail.com>
2025-04-18 18:17:48 -0500

    try out  myst_heading_anchors

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -36,6 +36,7 @@ autodoc_default_options = {
 }
 
 myst_enable_extensions = ["html_image"]
+myst_heading_anchors = 2
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
